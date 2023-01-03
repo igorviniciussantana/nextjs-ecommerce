cf8ddd9c2f0277fe7d6ec9fa937ebb9ec3ef33d3
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-03 14:15:56 -0400

    feat: finish tailwind setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-       
+       <h1 className='text-3xl font-bold underline text-green-700'>Igor Clothes</h1>
       </main>
     </>
   )
