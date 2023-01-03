d0e90a40f56fc43ee4717f7cc716ee6ffad9b393
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-03 19:49:52 -0400

    chore: add paths

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Header(){
+return(
+    <h1 className='text-3xl font-bold text-green-700'>Igor Clothes</h1>
+)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Igor Clothes | Loja Online</title>
-        <meta name="description" content="Loja virtual com as melhores roupas do Brasil!" />
+        <meta
+          name="description"
+          content="Loja virtual com as melhores roupas do Brasil!"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='container mx-auto'>
-       <h1 className='text-3xl font-bold text-green-700'>Igor Clothes</h1>
+      <main className="container mx-auto">
+        <h1 className="text-3xl font-bold text-green-700">Igor Clothes</h1>
       </main>
     </>
-  )
+  );
 }
