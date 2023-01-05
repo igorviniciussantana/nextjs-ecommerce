a098380b7a7f46563f3cb31a7c8dc4426c44e265
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-05 18:04:42 -0400

    fix: change styled-icons to phosphor-icons

--- src/components/HeaderProfile/index.tsx
+++ src/components/HeaderProfile/index.tsx
@@ -1,19 +1,18 @@
 import Link from "next/link";
-import { Search, User} from '@styled-icons/feather'
-import { Handbag } from '@styled-icons/bootstrap'
+import { MagnifyingGlass, User, Handbag} from 'phosphor-react'
 import * as Styled from "./styled";
 
 export default function HeaderProfile() {
   return (
     <Styled.HeaderProfile>
      <Styled.Box>
-      <Search size='30' className="hover:text-purple-800"/>
+      <MagnifyingGlass size='30' weight="light"  className="hover:text-purple-800"/>
      </Styled.Box>
      <Styled.Box>
-      <User size='30' className="hover:text-purple-800"/>
+      <User size='30' weight="light"  className="hover:text-purple-800"/>
      </Styled.Box>
      <Styled.Box>
-      <Handbag size='30' className="hover:text-purple-800"/>
+      <Handbag size='30'  weight="light" className="hover:text-purple-800"/>
      </Styled.Box>
     </Styled.HeaderProfile>
   );
