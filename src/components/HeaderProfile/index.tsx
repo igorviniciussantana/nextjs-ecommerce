340eaa2a1831473da06ff520359a8830d45bcd88
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-05 10:07:18 -0400

    feat: add search icon

--- src/components/HeaderProfile/index.tsx
+++ src/components/HeaderProfile/index.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { Search } from '@styled-icons/feather'
 import * as Styled from "./styled";
 
 export default function HeaderProfile() {
   return (
     <Styled.HeaderProfile>
-     <Styled.Box></Styled.Box>
+     <Styled.Box>
+      <Search size='30'/>
+     </Styled.Box>
     </Styled.HeaderProfile>
   );
 }
