8268c7480fdd250e39468ba15046523e229db49b
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-05 12:46:23 -0400

    feat: add card props types

--- src/components/Products/Card/index.tsx
+++ src/components/Products/Card/index.tsx
@@ -1,13 +1,24 @@
 import * as Styled from './styled'
 import Image from 'next/image'
 
+interface CardProps{
+    src: string;
+    alt: string;
+    img: string;
+    width: number;
+    height: number;
+}
 
-export default function Card(){
+export default function Card(props : CardProps){
 return(
     <Styled.Card>
         <Styled.Box>
             <Image 
-            
+            className='img-fallback'
+            src={props.img}
+            alt={props.alt}
+            width={props.width}
+            height={props.height}
             fill
             />
         </Styled.Box>
