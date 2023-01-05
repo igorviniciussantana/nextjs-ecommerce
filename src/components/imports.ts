4b0ba4c6dd16d5dfaa9e829ddf81d061d47cf64d
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-05 10:28:22 -0400

    feat: create `Card` component

--- src/components/imports.ts
+++ src/components/imports.ts
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import Nav from "./Nav";
 import HeaderProfile from "./HeaderProfile";
+import Card from './Products/Card'
 
 
 export{
     Header,
     Nav,
-    HeaderProfile
+    HeaderProfile,
+    Card
 }
