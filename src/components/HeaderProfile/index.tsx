95161f2c8c59dfa0a38b94ed3722e933aef1e4e9
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-05 10:18:15 -0400

    feat: finish Header Profile styles

--- src/components/HeaderProfile/index.tsx
+++ src/components/HeaderProfile/index.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
-import { Search } from '@styled-icons/feather'
+import { Search, User} from '@styled-icons/feather'
+import { Handbag } from '@styled-icons/bootstrap'
 import * as Styled from "./styled";
 
 export default function HeaderProfile() {
   return (
     <Styled.HeaderProfile>
      <Styled.Box>
-      <Search size='30'/>
+      <Search size='30' className="hover:text-purple-800"/>
+     </Styled.Box>
+     <Styled.Box>
+      <User size='30' className="hover:text-purple-800"/>
+     </Styled.Box>
+     <Styled.Box>
+      <Handbag size='30' className="hover:text-purple-800"/>
      </Styled.Box>
     </Styled.HeaderProfile>
   );
--- src/components/HeaderProfile/styled.ts
+++ src/components/HeaderProfile/styled.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-export const HeaderProfile = styled.div``;
+export const HeaderProfile = styled.div`
+display: flex;
+gap: 1rem;
+`;
 
 export const Box = styled.div`
 position: relative;
