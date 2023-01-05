eabe317a868f2f0037c1aa5b8fe4ea58270d6d32
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-05 12:47:36 -0400

    style: auto format card index

--- src/components/Products/Card/index.tsx
+++ src/components/Products/Card/index.tsx
@@ -1,27 +1,27 @@
-import * as Styled from './styled'
-import Image from 'next/image'
+import * as Styled from "./styled";
+import Image from "next/image";
 
-interface CardProps{
-    src: string;
-    alt: string;
-    img: string;
-    width: number;
-    height: number;
+interface CardProps {
+  src: string;
+  alt: string;
+  img: string;
+  width: number;
+  height: number;
 }
 
-export default function Card(props : CardProps){
-return(
+export default function Card(props: CardProps) {
+  return (
     <Styled.Card>
-        <Styled.Box>
-            <Image 
-            className='img-fallback'
-            src={props.img}
-            alt={props.alt}
-            width={props.width}
-            height={props.height}
-            fill
-            />
-        </Styled.Box>
+      <Styled.Box>
+        <Image
+          className="img-fallback"
+          src={props.img}
+          alt={props.alt}
+          width={props.width}
+          height={props.height}
+          fill
+        />
+      </Styled.Box>
     </Styled.Card>
-)
+  );
 }
