81df974c348a2285bf02357fc2b47ffa3706c81f
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-06 12:23:16 -0400

    feat: add SliderList props types

--- src/components/Products/SliderList/index.tsx
+++ src/components/Products/SliderList/index.tsx
@@ -2,14 +2,14 @@ import { Heading, Section, TitleBox } from "@components/imports";
 import * as Styled from "./styled";
 
 interface SliderList{
-  
+  title: string;
 }
 
-export default function SliderList() {
+export default function SliderList(props : SliderList) {
   return (
     <Section className="mx-auto">
       <TitleBox>
-        <Heading>
+        <Heading type='h1' className="font-medium">
           {props.title}
         </Heading>
       </TitleBox>
