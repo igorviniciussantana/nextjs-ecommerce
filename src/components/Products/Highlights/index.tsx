65e613ba672538759da5530a36bc525d0566f4dd
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-08 16:40:13 -0400

    feat: add getStaticProps to Highlights

--- src/components/Products/Highlights/index.tsx
+++ src/components/Products/Highlights/index.tsx
@@ -1,13 +1,25 @@
 import { Card } from "@components/imports";
 import * as Styled from "./styled";
-
+import { api } from "@api/api";
+import { GetStaticProps } from "next";
 
 export default function Highlights() {
   return (
     <Styled.Highlights className="mx-auto">
       <Card img='/camiseta1.webp' alt='Camiseta' width={720} height={1080} title='Camiseta Clean' price={99.90}/>
-      <Card img='/camiseta2.webp' alt='Camiseta' width={720} height={1080} title='Camiseta Retro' price={99.90}/>
-      <Card img='/camiseta3.webp' alt='Camiseta' width={720} height={1080} title='Camiseta Vintage' price={99.90}/>
     </Styled.Highlights>
   );
 }
+
+
+export const getStaticProps : GetStaticProps = async () => {
+
+   const highlightsResponse = await api.get(`/products?filters[highlight][$eq]=true&populate=*`)
+
+  return {
+    props: {
+      highlights: highlightsResponse.data
+    },
+    revalidate: 20
+  };
+};
